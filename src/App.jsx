950240489fce865f4e8a4f6d4f2b9e960e9b207b
AnishPowar01 <powaranish74@gmail.com>
2023-03-16 10:16:56 +0530

    Added- 3D- Model

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {Hero, Navbar} from './components'
+
+
+const App = () => {
+
+
+  return (
+    <BrowserRouter>
+    <div className="realtive z-0 bg-primary">
+    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+
+    <Navbar/>
+    <Hero/>
+  
+    </div>
+
+    </div>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Hello this is model
+    </div>
+  )
+}
+
+export default About
